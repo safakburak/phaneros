f6889b5d7ecace1fc3bd9bab7398c62209093439
Şafak Burak Çevikbaş <safak.cevikbas@t2.com.tr>
2017-02-14 12:37:47 +0300

    farklı hızlar için destek verildi

--- p2p/src/p2p/common/Walker.java
+++ p2p/src/p2p/common/Walker.java
@@ -9,6 +9,10 @@ public abstract class Walker {
 	protected int worldWidth;
 	protected int worldHeight;
 
+	private Point lastMove;
+
+	private float speed = 4;
+
 	public Walker(AbstractAgent<?> agent, int worldWidth, int worldHeight) {
 
 		this.agent = agent;
@@ -22,18 +26,54 @@ public abstract class Walker {
 
 		float t = time / 1000; // to seconds
 
-		t = 1;
+		if (Math.abs(((int) agent.getX()) - agent.getX()) > 0.01
+				|| Math.abs(((int) agent.getY()) - agent.getY()) > 0.01) {
 
-		Point move = new Point();
-
-		if (updateDirection(move)) {
-
-			agent.setPosition(agent.getX() + move.x * t, agent.getY() + move.y * t);
+			agent.setPosition(agent.getX() + lastMove.x * t * speed, agent.getY() + lastMove.y * t * speed);
 			agent.onPositionChange();
 
 		} else {
 
-			agent.onCacheMiss(agent.getX() + move.x * t, agent.getY() + move.y * t);
+			Point move = new Point();
+
+			if (updateDirection(move)) {
+
+				float dX = move.x * t * speed;
+				float dY = move.y * t * speed;
+
+				while (isInBounds(agent.getX() + dX, 0) == false) {
+
+					if (dX > 0.01) {
+
+						dX -= 1;
+
+					} else if (dX < -0.01) {
+
+						dX += 1;
+					}
+				}
+
+				while (isInBounds(0, agent.getY() + dY) == false) {
+
+					if (dY > 0.01) {
+
+						dY -= 1;
+
+					} else if (dY < -0.01) {
+
+						dY += 1;
+					}
+				}
+
+				agent.setPosition(agent.getX() + dX, agent.getY() + dY);
+				agent.onPositionChange();
+
+				lastMove = move;
+
+			} else {
+
+				agent.onCacheMiss(agent.getX() + move.x * t, agent.getY() + move.y * t);
+			}
 		}
 	}
 
